7fd223654664b1999bdbd020f1a33351e0b8b40c
tiwansh <at.slimshady@gmail.com>
2017-05-30 14:43:40 +0530

    Second Commit - Created Maps using API

--- popup.js
+++ popup.js
@@ -1,10 +1,36 @@
-(function(){
+function initMap(){
+	map = new google.maps.Map(document.getElementById('dispmap'),{
+		center : {lat:-34.397, lng:150.644},
+		zoom: 6
+	});
 
-	function onPositionReceived(position){
-		console.log(position);
-	}
+	infoWindow = new google.maps.InfoWindow;
 
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(onPositionReceived);
+		navigator.geolocation.getCurrentPosition(function(position){
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			infoWindow.setPosition(pos);
+			infoWindow.setContent("Location Found!!");
+			infoWindow.open(map);
+			map.setCenter(pos);
+		},
+		function(){
+			handleLocationError(true, infoWindow, map.getCenter());
+
+		});
 	}
-})();
+	else{
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+  }
